ba33ebb28b8cadada8942494ad75140585eea07c
ScoutOK <ohkeefe@gmail.com>
2017-06-04 16:37:25 -0400

    finished basic schema

---
+++ schema.js
@@ -0,0 +1,91 @@
+import graphql, {GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList, GraphQLSchema} from 'graphql';
+import db from './db';
+
+const Person = new GraphQLObjectType({
+  name: 'Person',
+  description: 'This represents a Person',
+  fields: () => {
+    return {
+      id: {
+        type: GraphQLInt,
+        resolve(person) {
+          return person.id
+        }
+      },
+      firstName: {
+        type: GraphQLString,
+        resolve(person) {
+          return person.firstName
+        }
+      },
+      lastName: {
+        type: GraphQLString,
+        resolve(person) {
+          return person.lastName
+        }
+      },
+      email: {
+        type: GraphQLString,
+        resolve(person) {
+          return person.email
+        }
+      }
+    }
+  }
+})
+
+const Post = new GraphQLObjectType({
+  name: 'Post',
+  description: 'This represents a post',
+  fields: ()=> {
+    return {
+      id: {
+        type: GraphQLInt,
+        resolve(post) {
+          return post.id
+        }
+      },
+      title: {
+        type: GraphQLString,
+        resolve(post) {
+          return post.title
+        }
+      },
+      content: {
+        type: GraphQLString,
+        resolve(post) {
+          return post.content
+        }
+      }
+    }
+  }
+})
+
+const Query = new GraphQLObjectType({
+  name: 'Query',
+  description: 'This is a root query',
+  fields: () => {
+    return {
+      people: {
+        type: new GraphQLList(Person),
+        args: {//this part is to limit what arguments users can send to the database
+          id: {
+            type: GraphQLInt
+          },
+          email: {
+            type: GraphQLString
+          }
+        },
+        resolve(root, args) {
+          return db.models.person.findAll({where: args})
+        }
+      }
+    }
+  }
+})
+
+const Schema = new GraphQLSchema({
+  query: Query
+});
+
+export default Schema;
